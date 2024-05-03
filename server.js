a9ffaed41cc4d7738294e0bbccb131c3dd0f81e5
chojeongeun <juy0816@naver.com>
2024-05-03 18:20:13 +0900

    post요청 & express.json()

--- server.js
+++ server.js
@@ -14,12 +14,11 @@ const Users = [
 ];
 
 const app = express();
-
+app.use(express.json());
 app.use((req, res, next) => {
   const start = Date.now();
   console.log(`start: ${req.method} ${req.url}`);
   next();
-
   const diffTime = Date.now() - start;
   console.log(`end: ${req.method} ${req.url} ${diffTime}ms`);
 });
@@ -30,6 +29,23 @@ app.get("/users", (req, res) => {
 
 app.get("/", (req, res) => {
   res.send("Hello, World");
+});
+
+//post로 배열 추가하기
+app.post("/users", (req, res) => {
+  //에러가 나서 끊겼을땐 return으로 끊어주기
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: "Missing user name",
+    });
+  }
+
+  const newUser = {
+    name: req.body.name,
+    id: Users.length,
+  };
+  Users.push(newUser);
+  res.json(newUser);
 });
 
 app.get("/users/:userId", (req, res) => {

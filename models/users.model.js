bc6acfc3d3448a331c3fecf6a0385daa769fddbe
chojeongeun <juy0816@naver.com>
2024-05-04 15:59:22 +0900

    MVC 패턴으로 변경

---
+++ models/users.model.js
@@ -0,0 +1,12 @@
+const Users = [
+  {
+    id: 0,
+    name: "Jack",
+  },
+  {
+    id: 1,
+    name: "Jannifer",
+  },
+];
+
+module.exports = Users;
--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 const express = require("express");
-
+const usersController = require("./controllers/users.controller");
+const postsController = require("./controllers/posts.controller");
 const PORT = 4000;
-
-const Users = [
-  {
-    id: 0,
-    name: "Jack",
-  },
-  {
-    id: 1,
-    name: "Jannifer",
-  },
-];
 
 const app = express();
 app.use(express.json());
@@ -23,40 +13,11 @@ app.use((req, res, next) => {
   console.log(`end: ${req.method} ${req.url} ${diffTime}ms`);
 });
 
-app.get("/users", (req, res) => {
-  res.send(Users);
-});
+app.get("/users", usersController.getUsers);
+app.get("/users/:userId", usersController.getUser);
+app.post("/users", usersController.postUser);
 
-app.get("/", (req, res) => {
-  res.send("Hello, World");
-});
-
-//post로 배열 추가하기
-app.post("/users", (req, res) => {
-  //에러가 나서 끊겼을땐 return으로 끊어주기
-  if (!req.body.name) {
-    return res.status(400).json({
-      error: "Missing user name",
-    });
-  }
-
-  const newUser = {
-    name: req.body.name,
-    id: Users.length,
-  };
-  Users.push(newUser);
-  res.json(newUser);
-});
-
-app.get("/users/:userId", (req, res) => {
-  const userId = Number(req.params.userId);
-  const user = Users[userId];
-  if (user) {
-    res.json(user);
-  } else {
-    res.sendStatus(404);
-  }
-});
+app.get("/posts", postsController.getPost);
 
 app.listen(PORT, () => {
   console.log(`Running port on ${PORT}`);

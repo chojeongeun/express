7f3c933920f4d7a5ad1c6dd76e0d11938feb5c7b
chojeongeun <juy0816@naver.com>
2024-05-04 16:38:45 +0900

    Express에서 파일 전송

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -2,6 +2,6 @@ const express = require("express");
 const postsRouter = express.Router();
 const postsController = require("../controllers/posts.controller");
 
-postsRouter.get("/posts", postsController.getPost);
+postsRouter.get("/", postsController.getPost);
 
 module.exports = postsRouter;

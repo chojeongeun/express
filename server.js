cbdd12867471b9720df8b63ce5a3ab3b3a45b677
chojeongeun <juy0816@naver.com>
2024-05-04 17:16:23 +0900

    Template Engine 사용

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require("path");
 const usersRouter = require("./routes/users.router");
 const postsRouter = require("./routes/posts.router");
 const app = express();
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use("/static", express.static(path.join(__dirname, "public")));
 
@@ -14,6 +16,12 @@ app.use((req, res, next) => {
   next();
   const diffTime = Date.now() - start;
   console.log(`end: ${req.method} ${req.baseUrl} ${req.url} ${diffTime}ms`);
+});
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    imageTitle: "It is a dog 2",
+  });
 });
 
 app.use("/users", usersRouter);
